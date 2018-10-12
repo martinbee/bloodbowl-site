de625cce8a93c7af329b6a0911d62841c93eb2a5
Martin Bee <martbee88@gmail.com>
2018-10-12 11:07:02 -0500

    feat: init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import 'semantic-ui-css/semantic.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ui text container">
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/RacePage/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RacePage = () => (
+  <div>Race Page</div>
+);
+
+export default RacePage;
