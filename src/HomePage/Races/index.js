eb729ff396bb7150231a0ff45a94106af82bfd83
Martin Bee <martbee88@gmail.com>
2018-10-12 14:46:21 -0500

    feat: set up homepage skeleton

---
+++ src/HomePage/Races/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Races = () => <div>Races</div>;
+
+export default Races;
--- src/HomePage/index.js
+++ src/HomePage/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Header from './Header';
+import Jumbotron from './Jumbotron';
+import Races from './Races';
 
-const HomePage = () => (
-  <div>Home Page</div>
-);
+const HomePage = () => ([
+  <Header key="header" />,
+  <Jumbotron key="jumbotron" />,
+  <Races key="races" />,
+]);
 
 export default HomePage;
