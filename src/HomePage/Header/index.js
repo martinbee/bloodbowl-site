1d0add7a125216172c14d89281d5e610fa7991ff
Martin Bee <martbee88@gmail.com>
2018-10-12 15:00:27 -0500

    feat: begin header work

--- src/HomePage/Header/index.js
+++ src/HomePage/Header/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Dropdown, Menu  } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 
-const Header = () => <div>Header</div>;
+const Header = () => (
+  <Menu inverted fixed>
+    <Menu.Item header>Blood Bowl</Menu.Item>
+    <Dropdown item text='Races'>
+      <Dropdown.Menu>
+        <Link to="/races">
+          <Dropdown.Item color="black">All</Dropdown.Item>
+        </Link>
+      </Dropdown.Menu>
+    </Dropdown>
+  </Menu>
+);
 
 export default Header;
